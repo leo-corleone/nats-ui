46c4c552b6793375aae08779d8a6006ecd8f0557
suen <1499718389@qq.com>
2023-06-10 22:22:40 +0800

    add ViewConstant.java

--- src/main/java/org/suen/MainApplication.java
+++ src/main/java/org/suen/MainApplication.java
@@ -2,17 +2,38 @@ package org.suen;
 
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
+import org.suen.constant.ViewConstant;
+
+import java.awt.event.ActionEvent;
+import java.util.Optional;
 
 @Slf4j
 public class MainApplication extends Application {
     public static void main(String[] args) {
         launch(args);
+    }
+
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        log.info("init...");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        log.info("stop...");
     }
 
     @Override
@@ -27,7 +48,23 @@ public class MainApplication extends Application {
         primaryStage.show();
         primaryStage.setHeight(600);
         primaryStage.setWidth(900);
-        primaryStage.getIcons().add(new Image("/image/logo.png" ));
+        primaryStage.getIcons().add(new Image(ViewConstant.LOGO));
 
+        log.info("start...");
+
+        primaryStage.setOnCloseRequest(event ->{
+            event.consume();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("退出程序");
+            alert.setHeaderText("xxxx");
+            alert.setContentText("是否退出程序");
+            Stage alertStage = (Stage) alert.getDialogPane().getScene().getWindow();
+            alertStage.getIcons().add(new Image(ViewConstant.LOGO));
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                Platform.exit();
+            }
+        });
     }
 }
---
+++ src/main/java/org/suen/constant/ViewConstant.java
@@ -0,0 +1,16 @@
+package org.suen.constant;
+
+/**
+ * @author: suen
+ * @time: 2023/6/10
+ * @description: 视图常量
+ **/
+public class ViewConstant {
+
+
+    /**
+     * 全局logo图标
+     */
+    public static final String LOGO = "/image/logo.png";
+
+}
